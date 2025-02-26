9fb3c689d4ea9e8cdd87f509100604eb14b501c8
Ironmomo <andrin.fassbind@gmail.com>
2025-02-26 21:08:09 +0100

    add db/testcontainers configuration
    -- connection/performance config

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestDBConfiguration.class)
 class BackendApplicationTests {
 
     @Test
