29144baef07222d79a79f2b3ed4fa861c5f1664e
Ironmomo <andrin.fassbind@gmail.com>
2025-03-31 20:37:34 +0200

    use serial instead of bigserial

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,24 +1,25 @@
 DROP TABLE IF EXISTS users_boats;
 DROP TABLE IF EXISTS boats;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reservations;
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id       BIGSERIAL PRIMARY KEY,
+    id       SERIAL PRIMARY KEY,
     username VARCHAR(25) NOT NULL UNIQUE,
     password VARCHAR     NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS boats
 (
-    id   BIGSERIAL PRIMARY KEY,
+    id   SERIAL PRIMARY KEY,
     name VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS users_boats
 (
-    user_id BIGSERIAL NOT NULL,
-    boat_id BIGSERIAL NOT NULL,
+    user_id SERIAL NOT NULL,
+    boat_id SERIAL NOT NULL,
     PRIMARY KEY (user_id),
     CONSTRAINT fk_users_boats_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_users_boats_boat FOREIGN KEY (boat_id) REFERENCES boats (id) ON DELETE CASCADE
@@ -26,13 +27,13 @@ CREATE TABLE IF NOT EXISTS users_boats
 
 CREATE TABLE IF NOT EXISTS reservations
 (
-    id                  BIGSERIAL PRIMARY KEY,
+    id                  SERIAL PRIMARY KEY,
     start_date          TIMESTAMP NOT NULL,
     end_date            TIMESTAMP NOT NULL,
     boat_hours_on_start INTEGER,
     boat_hours_on_end   INTEGER,
-    boat_id             BIGSERIAL NOT NULL,
-    user_id             BIGSERIAL NOT NULL,
+    boat_id             SERIAL    NOT NULL,
+    user_id             SERIAL    NOT NULL,
     CONSTRAINT fk_reservations_boat FOREIGN KEY (boat_id) REFERENCES boats (id),
     CONSTRAINT fk_reservation_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
