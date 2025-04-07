657d331cbee7889576b41ae4f5548b02a0620698
Ironmomo <andrin.fassbind@gmail.com>
2025-04-07 19:18:04 +0200

    generate api stubs/services

---
+++ frontend/src/app/api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './authenticationRequestTO';
+export * from './authenticationResponseTO';
+export * from './boatId';
+export * from './createReservationDTO';
+export * from './reservationId';
+export * from './updateEngineHoursDTO';
+export * from './userDTO';
