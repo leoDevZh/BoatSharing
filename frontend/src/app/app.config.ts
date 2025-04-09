2198513ba6c576ddffbc29dd58315cf14d36625f
Ironmomo <andrin.fassbind@gmail.com>
2025-04-09 21:28:01 +0200

    set base path

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {BASE_PATH} from './api';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: BASE_PATH,
+      useValue: 'http://localhost:8080'
+    }
+  ]
 };
