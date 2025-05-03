04366e38085409e4de1f85766a820cd89bf79808
Ironmomo <andrin.fassbind@gmail.com>
2025-05-03 11:37:07 +0200

    refresh token on initial render

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {ToastyComponent} from './toasty/toasty.component';
+import {AuthenticationControllerService} from './api';
+import {AuthService} from './services/AuthService/auth.service';
 
 @Component({
   selector: 'bs-root',
@@ -9,5 +11,13 @@ import {ToastyComponent} from './toasty/toasty.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private authController = inject(AuthenticationControllerService)
+  private authService = inject(AuthService)
+
+  ngOnInit(): void {
+    this.authController.refresh().subscribe(response => {
+      this.authService.setToken(response.token)
+    })
+  }
 }
