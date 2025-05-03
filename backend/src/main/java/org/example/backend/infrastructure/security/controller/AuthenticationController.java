ebd229c59e0596752dd8d2b76f713acdddce1c5e
Ironmomo <andrin.fassbind@gmail.com>
2025-05-03 10:51:51 +0200

    implement token refresh endpoint

--- backend/src/main/java/org/example/backend/infrastructure/security/controller/AuthenticationController.java
+++ backend/src/main/java/org/example/backend/infrastructure/security/controller/AuthenticationController.java
@@ -1,9 +1,11 @@
 package org.example.backend.infrastructure.security.controller;
 
 import org.example.backend.infrastructure.security.jwt.JwtUtil;
+import org.example.backend.infrastructure.security.user.CustomUserDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,13 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponseTO> authenticate(@RequestBody AuthenticationRequestTO request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
         String token = JwtUtil.generateToken(request.getUsername());
+        return ResponseEntity.ok(new AuthenticationResponseTO(token));
+    }
+
+    @PostMapping(value = "/refresh", produces = "application/json")
+    public ResponseEntity<AuthenticationResponseTO> refresh() {
+        CustomUserDetail userDetail = (CustomUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String token = JwtUtil.generateToken(userDetail.getUsername());
         return ResponseEntity.ok(new AuthenticationResponseTO(token));
     }
 }
--- backend/src/test/java/org/example/backend/integration/security/AuthenticationTest.java
+++ backend/src/test/java/org/example/backend/integration/security/AuthenticationTest.java
@@ -5,7 +5,9 @@ import org.example.backend.TestDBConfiguration;
 import org.example.backend.infrastructure.repository.user.JpaUserRepository;
 import org.example.backend.infrastructure.repository.user.User;
 import org.example.backend.infrastructure.security.controller.AuthenticationRequestTO;
+import org.example.backend.infrastructure.security.user.CustomUserDetail;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,6 +21,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.UUID;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,6 +43,7 @@ public class AuthenticationTest {
     private ObjectMapper objectMapper;
 
     private User user;
+    private CustomUserDetail customUserDetail;
 
     @BeforeEach
     void setup() {
@@ -48,47 +52,73 @@ public class AuthenticationTest {
                 .password("$2a$12$GbqlJ0fs.00G5MLiCA9AZuA0QF000rXCrkQ0sP5EkGbwsOb.KmlEm") // password123
                 .build();
         userRepository.save(user);
+        customUserDetail = CustomUserDetail.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .build();
     }
 
-    @Test
-    void authenticateUserOnSuccess() throws Exception {
-        AuthenticationRequestTO authenticationRequestTO = new AuthenticationRequestTO();
-        authenticationRequestTO.setUsername(user.getUsername());
-        authenticationRequestTO.setPassword("password123");
-        String authenticationRequestJson = objectMapper.writeValueAsString(authenticationRequestTO);
+    @Nested
+    class AuthenticateTests {
+        @Test
+        void authenticateUserOnSuccess() throws Exception {
+            AuthenticationRequestTO authenticationRequestTO = new AuthenticationRequestTO();
+            authenticationRequestTO.setUsername(user.getUsername());
+            authenticationRequestTO.setPassword("password123");
+            String authenticationRequestJson = objectMapper.writeValueAsString(authenticationRequestTO);
 
-        mockMvc.perform(post("/api/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(authenticationRequestJson))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token").exists());
+            mockMvc.perform(post("/api/auth/login")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(authenticationRequestJson))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.token").exists());
+        }
+
+        @Test
+        void authenticateUserOnWrongPassword() throws Exception {
+            AuthenticationRequestTO authenticationRequestTO = new AuthenticationRequestTO();
+            authenticationRequestTO.setUsername(user.getUsername());
+            authenticationRequestTO.setPassword("someOtherPassword");
+            String authenticationRequestJson = objectMapper.writeValueAsString(authenticationRequestTO);
+
+            mockMvc.perform(post("/api/auth/login")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(authenticationRequestJson))
+                    .andExpect(status().isUnauthorized())
+                    .andExpect(jsonPath("$.token").doesNotExist());
+        }
+
+        @Test
+        void authenticateUserOnWrongUsername() throws Exception {
+            AuthenticationRequestTO authenticationRequestTO = new AuthenticationRequestTO();
+            authenticationRequestTO.setUsername("otherUser");
+            authenticationRequestTO.setPassword("password123");
+            String authenticationRequestJson = objectMapper.writeValueAsString(authenticationRequestTO);
+
+            mockMvc.perform(post("/api/auth/login")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(authenticationRequestJson))
+                    .andExpect(status().isUnauthorized())
+                    .andExpect(jsonPath("$.token").doesNotExist());
+        }
     }
 
-    @Test
-    void authenticateUserOnWrongPassword() throws Exception {
-        AuthenticationRequestTO authenticationRequestTO = new AuthenticationRequestTO();
-        authenticationRequestTO.setUsername(user.getUsername());
-        authenticationRequestTO.setPassword("someOtherPassword");
-        String authenticationRequestJson = objectMapper.writeValueAsString(authenticationRequestTO);
+    @Nested
+    class RefreshTests {
+        @Test
+        void refreshOnSuccess() throws Exception {
+            mockMvc.perform(post("/api/auth/refresh")
+                            .with(user(customUserDetail)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.token").exists());
+        }
 
-        mockMvc.perform(post("/api/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(authenticationRequestJson))
-                .andExpect(status().isUnauthorized())
-                .andExpect(jsonPath("$.token").doesNotExist());
-    }
-
-    @Test
-    void authenticateUserOnWrongUsername() throws Exception {
-        AuthenticationRequestTO authenticationRequestTO = new AuthenticationRequestTO();
-        authenticationRequestTO.setUsername("otherUser");
-        authenticationRequestTO.setPassword("password123");
-        String authenticationRequestJson = objectMapper.writeValueAsString(authenticationRequestTO);
-
-        mockMvc.perform(post("/api/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(authenticationRequestJson))
-                .andExpect(status().isUnauthorized())
-                .andExpect(jsonPath("$.token").doesNotExist());
+        @Test
+        void refreshOnNotAuthenticated() throws Exception {
+            mockMvc.perform(post("/api/auth/refresh"))
+                    .andExpect(status().isForbidden())
+                    .andExpect(jsonPath("$.token").doesNotExist());
+        }
     }
 }
