2447a7ddd0505741c50935d1a0a4db83b2ab439d
Ironmomo <andrin.fassbind@gmail.com>
2025-05-16 15:10:23 +0200

    implement user resolver

--- frontend/src/app/resolver/user.resolver.spec.ts
+++ frontend/src/app/resolver/user.resolver.spec.ts
@@ -1,24 +1,35 @@
 import {TestBed} from '@angular/core/testing';
-import {ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, ResolveFn, Router, RouterStateSnapshot} from '@angular/router';
 
 import {userResolver} from './user.resolver';
 import {UserControllerService, UserDTO} from '../api';
+import {Observable, of, throwError} from 'rxjs';
+import {ToastyService} from '../services/toasty/toasty.service';
 
-fdescribe('userResolver', () => {
+describe('userResolver', () => {
   let userService: jasmine.NonTypedSpyObj<UserControllerService>
   let route: jasmine.SpyObj<ActivatedRouteSnapshot>
   let state: jasmine.SpyObj<RouterStateSnapshot>
+  let router: jasmine.SpyObj<Router>
+  let toastyService: jasmine.SpyObj<ToastyService>
 
   const executeResolver: ResolveFn<UserDTO> = (...resolverParameters) =>
     TestBed.runInInjectionContext(() => userResolver(...resolverParameters));
 
   beforeEach(() => {
     userService = jasmine.createSpyObj('UserControllerService', ['getUser'])
+    userService.getUser.and.returnValue(of({username: "user"} as UserDTO))
     route = jasmine.createSpyObj('ActivatedRouteSnapshot', [], {params: {}})
     state = jasmine.createSpyObj('RouterStateSnapshot', [], {url: '/'})
+    router = jasmine.createSpyObj('Router', ['navigate'])
+    toastyService = jasmine.createSpyObj('ToastyService', ['addErrorNotification'])
 
     TestBed.configureTestingModule({
-      providers: [{provide: UserControllerService, useValue: userService}]
+      providers: [
+        {provide: UserControllerService, useValue: userService},
+        {provide: Router, useValue: router},
+        {provide: ToastyService, useValue: toastyService}
+      ]
     });
   });
 
@@ -26,8 +37,22 @@ fdescribe('userResolver', () => {
     expect(executeResolver).toBeTruthy();
   });
 
-  it('should resolve user data from the service', () => {
-    executeResolver(route, state)
-    expect(userService.getUser).toHaveBeenCalled()
+  it('should resolve user data from the service', async () => {
+    const res$ = await executeResolver(route, state) as Observable<UserDTO>
+    res$.subscribe({
+      next: () => expect(userService.getUser).toHaveBeenCalled()
+    })
   });
+
+  it('should handle error', async () => {
+    userService.getUser.and.returnValue(throwError(() => "some error"))
+    const res$ = await executeResolver(route, state) as Observable<UserDTO>
+    res$.subscribe({
+      next: () => fail('Expected resolver to throw an error'),
+      error: (err) => {
+        expect(router.navigate).toHaveBeenCalledWith(['/error'])
+        expect(toastyService.addErrorNotification).toHaveBeenCalled()
+      }
+    })
+  })
 });
--- frontend/src/app/resolver/user.resolver.ts
+++ frontend/src/app/resolver/user.resolver.ts
@@ -1,7 +1,20 @@
-import {ResolveFn} from '@angular/router';
+import {ResolveFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {UserControllerService, UserDTO} from '../api';
+import {catchError, Observable, throwError} from 'rxjs';
+import {ToastyService} from '../services/toasty/toasty.service';
 
-export const userResolver: ResolveFn<UserDTO> = (route, state) => {
-  return inject(UserControllerService).getUser();
+export const userResolver: ResolveFn<UserDTO> = (route, state): Observable<UserDTO> => {
+  const userService = inject(UserControllerService);
+  const router = inject(Router);
+  const toastyService = inject(ToastyService)
+
+  return userService.getUser()
+    .pipe(
+      catchError(err => {
+        toastyService.addErrorNotification("Unerwarteter Fehler: Userdaten konnten nicht geladen werden")
+        router.navigate(['/error'])
+        return throwError(() => err)
+      })
+    );
 };
