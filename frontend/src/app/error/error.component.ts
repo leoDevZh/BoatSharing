c6b721e88efefbeaef007c570d0cecdc57ce0130
Ironmomo <andrin.fassbind@gmail.com>
2025-05-16 15:09:22 +0200

    implement error component

---
+++ frontend/src/app/error/error.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-error',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.css'
+})
+export class ErrorComponent {
+
+}
