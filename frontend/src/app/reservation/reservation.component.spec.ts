8459a95a1d0f11f09c29b9bf080d389c183cbc80
Ironmomo <andrin.fassbind@gmail.com>
2025-05-21 23:13:18 +0200

    fetch reservations for selected month in reservation component

--- frontend/src/app/reservation/reservation.component.spec.ts
+++ frontend/src/app/reservation/reservation.component.spec.ts
@@ -1,16 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ReservationComponent } from './reservation.component';
+import {ReservationComponent} from './reservation.component';
+import {ActivatedRoute} from '@angular/router';
+import {of} from 'rxjs';
+import {BoatId} from '../api';
+import {ReservationService} from '../services/reservation/reservation.service';
+import {Component, model} from '@angular/core';
+import {DateTime} from 'luxon';
+
+@Component({
+  selector: 'bs-month-selector',
+  template: ''
+})
+class MonthSelectorStubComponent {
+  month = model<DateTime>(DateTime.now().startOf('month'))
+}
 
 describe('ReservationComponent', () => {
   let component: ReservationComponent;
   let fixture: ComponentFixture<ReservationComponent>;
+  let activatedRoute: jasmine.SpyObj<ActivatedRoute>
+  let reservationService: jasmine.SpyObj<ReservationService>
+  const boatId: BoatId = {value: 1}
 
   beforeEach(async () => {
+    reservationService = jasmine.createSpyObj('ReservationService', ['findReservationForPeriod'])
+    activatedRoute = jasmine.createSpyObj('ActivatedRoute', [], {data: of({boatId})})
     await TestBed.configureTestingModule({
-      imports: [ReservationComponent]
+      imports: [ReservationComponent],
+      providers: [
+        {provide: ActivatedRoute, useValue: activatedRoute},
+        {provide: ReservationService, useValue: reservationService}
+      ]
     })
-    .compileComponents();
+      .overrideComponent(ReservationComponent, {
+        set: {
+          imports: [MonthSelectorStubComponent]
+        }
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(ReservationComponent);
     component = fixture.componentInstance;
@@ -20,4 +48,18 @@ describe('ReservationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load boatId on Init', () => {
+    expect(component.boatId).toEqual(boatId)
+  })
+
+  it('should initialize date signals with current month by default', () => {
+    expect(component.firstDayOfActiveMonth().toFormat('yyyy-MM-dd')).toBe(DateTime.now().startOf('month').toFormat('yyyy-MM-dd'))
+    expect(component.daysOfMonth().at(0)?.toFormat('yyyy-MM-dd')).toBe(DateTime.now().startOf('month').startOf('week').toFormat('yyyy-MM-dd'))
+    expect(component.daysOfMonth().at(-1)?.toFormat('yyyy-MM-dd')).toBe(DateTime.now().endOf('month').endOf('week').toFormat('yyyy-MM-dd'))
+  })
+
+  it('should call service on init', () => {
+    expect(reservationService.findReservationForPeriod).toHaveBeenCalledWith(component.daysOfMonth().at(0)!, component.daysOfMonth().at(-1)!, component.boatId)
+  })
 });
--- frontend/src/app/reservation/reservation.component.ts
+++ frontend/src/app/reservation/reservation.component.ts
@@ -1,6 +1,9 @@
-import {DateTime} from 'luxon';
-import {Component, signal} from '@angular/core';
+import {DateTime, Interval} from 'luxon';
+import {Component, computed, effect, inject, OnInit, signal} from '@angular/core';
 import {MonthSelectorComponent} from './month-selector/month-selector.component';
+import {ActivatedRoute} from '@angular/router';
+import {BoatId} from '../api';
+import {ReservationService} from '../services/reservation/reservation.service';
 
 @Component({
   selector: 'bs-reservation',
@@ -10,6 +13,35 @@ import {MonthSelectorComponent} from './month-selector/month-selector.component'
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.css'
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit {
+
+  private activatedRoute = inject(ActivatedRoute);
+  private reservationService = inject(ReservationService)
+
+  boatId!: BoatId
   firstDayOfActiveMonth = signal(DateTime.now().startOf('month'))
+  daysOfMonth = computed(() => {
+    return Interval.fromDateTimes(
+      this.firstDayOfActiveMonth().startOf('week'),
+      this.firstDayOfActiveMonth().endOf('month').endOf('week')
+    )
+      .splitBy({day: 1})
+      .map(d => d.start)
+      .filter(d => d !== null)
+  })
+
+  constructor() {
+    effect(() => {
+      if (this.boatId === undefined) return
+      const from = this.daysOfMonth().at(0)!
+      const to = this.daysOfMonth().at(-1)!
+      this.reservationService.findReservationForPeriod(from, to, this.boatId)
+    })
+  }
+
+  ngOnInit() {
+    this.activatedRoute.data.subscribe(({boatId}) => {
+      this.boatId = boatId
+    })
+  }
 }
