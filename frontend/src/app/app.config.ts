2e2c2491d072fefb59c51f334229e1930560ddd1
Ironmomo <andrin.fassbind@gmail.com>
2025-06-03 20:58:24 +0200

    on the fly improve styling
    -- error toasty
    -- material form
    -- tabs

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,6 +7,7 @@ import {BASE_PATH} from './api';
 import {authInterceptor} from './interceptors/auth.interceptor';
 import {errorInterceptor} from './interceptors/error.interceptor';
 import {HAMMER_LOADER, HammerModule} from '@angular/platform-browser';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,6 +27,10 @@ export const appConfig: ApplicationConfig = {
     {
       provide: HAMMER_LOADER,
       useValue: () => import('hammerjs')
+    },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {appearance: 'outline', subscriptSizing: 'dynamic'}
     }
   ]
 };
