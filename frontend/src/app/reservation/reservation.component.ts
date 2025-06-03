beb234ebe12c389cf8acf9011a5b4d28c76b40e6
Ironmomo <andrin.fassbind@gmail.com>
2025-06-03 21:22:55 +0200

    display reservation function component

--- frontend/src/app/reservation/reservation.component.ts
+++ frontend/src/app/reservation/reservation.component.ts
@@ -2,13 +2,19 @@ import {DateTime, Interval} from 'luxon';
 import {Component, computed, effect, inject, OnInit, signal} from '@angular/core';
 import {MonthSelectorComponent} from './month-selector/month-selector.component';
 import {ActivatedRoute} from '@angular/router';
-import {BoatId} from '../api';
+import {BoatId, ReservationUserDTO} from '../api';
 import {ReservationService} from '../services/reservation/reservation.service';
+import {CalendarComponent} from './calendar/calendar.component';
+import {SelectedDayReservationsComponent} from './selected-day-reservations/selected-day-reservations.component';
+import {ReservationFormComponent} from './reservation-form/reservation-form.component';
 
 @Component({
   selector: 'bs-reservation',
   imports: [
-    MonthSelectorComponent
+    MonthSelectorComponent,
+    CalendarComponent,
+    SelectedDayReservationsComponent,
+    ReservationFormComponent
   ],
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.css'
@@ -22,20 +28,19 @@ export class ReservationComponent implements OnInit {
   firstDayOfActiveMonth = signal(DateTime.now().startOf('month'))
   daysOfMonth = computed(() => {
     return Interval.fromDateTimes(
-      this.firstDayOfActiveMonth().startOf('week'),
+      this.firstDayOfActiveMonth().startOf('month').startOf('week'),
       this.firstDayOfActiveMonth().endOf('month').endOf('week')
     )
       .splitBy({day: 1})
       .map(d => d.start)
       .filter(d => d !== null)
   })
+  selectedDay = signal<DateTime | undefined>(undefined)
+  reservationToUpdate = signal<ReservationUserDTO | undefined>(undefined)
 
   constructor() {
     effect(() => {
-      if (this.boatId === undefined) return
-      const from = this.daysOfMonth().at(0)!
-      const to = this.daysOfMonth().at(-1)!
-      this.reservationService.findReservationForPeriod(from, to, this.boatId)
+      this.reloadReservations();
     })
   }
 
@@ -43,5 +48,24 @@ export class ReservationComponent implements OnInit {
     this.activatedRoute.data.subscribe(({boatId}) => {
       this.boatId = boatId
     })
+  }
+
+  onSelectedDayChanged($event: DateTime) {
+    this.selectedDay.set($event)
+  }
+
+  onUpdateReservation($event: ReservationUserDTO) {
+    this.reservationToUpdate.set({...$event})
+  }
+
+  onReloadReservations() {
+    this.reloadReservations()
+  }
+
+  private reloadReservations() {
+    if (this.boatId === undefined) return
+    const from = this.daysOfMonth().at(0)!
+    const to = this.daysOfMonth().at(-1)!
+    this.reservationService.findReservationForPeriod(from, to, this.boatId)
   }
 }
