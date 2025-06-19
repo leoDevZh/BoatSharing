48b0afda606702ec209f462455252895a8d38713
Ironmomo <andrin.fassbind@gmail.com>
2025-06-19 15:40:00 +0200

    add payment to tab

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {TabMenuComponent, TabMenuItem} from '../shared/tab-menu/tab-menu.component';
 import {ReservationComponent} from '../reservation/reservation.component';
 import {TodoComponent} from '../todo/todo.component';
+import {PaymentComponent} from '../payment/payment.component';
 
 @Component({
   selector: 'bs-home',
@@ -14,6 +15,7 @@ import {TodoComponent} from '../todo/todo.component';
 export class HomeComponent {
   tabs: TabMenuItem[] = [
     {title: 'Ausfahrt', component: ReservationComponent},
-    {title: 'ToDo', component: TodoComponent}
+    {title: 'ToDo', component: TodoComponent},
+    {title: 'Zahlung', component: PaymentComponent}
   ];
 }
