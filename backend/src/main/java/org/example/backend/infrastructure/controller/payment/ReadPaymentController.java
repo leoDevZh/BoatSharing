dfc2f92dad4eb21272c144fcbb54ebe8d4c9ef70
Ironmomo <andrin.fassbind@gmail.com>
2025-06-21 20:52:26 +0200

    implement find payments from logged in user controller

---
+++ backend/src/main/java/org/example/backend/infrastructure/controller/payment/ReadPaymentController.java
@@ -0,0 +1,43 @@
+package org.example.backend.infrastructure.controller.payment;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.example.backend.domain.User.model.UserDTO;
+import org.example.backend.domain.User.model.UserId;
+import org.example.backend.domain.payment.api.ReadPaymentService;
+import org.example.backend.domain.payment.model.PaymentUserDTO;
+import org.example.backend.domain.shared.model.PagedResult;
+import org.example.backend.infrastructure.controller.excpetion.ExceptionDTO;
+import org.example.backend.infrastructure.security.user.CustomUserDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/read-payment")
+public class ReadPaymentController {
+
+    @Autowired
+    private ReadPaymentService readPaymentService;
+
+    @GetMapping(value = "all-from-logged-user", produces = "application/json")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Get all payments from logged in user. Paged"),
+            @ApiResponse(responseCode = "400", description = "Invalid reservation",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionDTO.class)))
+    })
+    public ResponseEntity<PagedResult<List<PaymentUserDTO>>> getpPaymentsFromLoggedInUser(@RequestParam int page) {
+        CustomUserDetail userDetail = (CustomUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        PagedResult<List<PaymentUserDTO>> data = readPaymentService.getPaymentsFromLoggedInUser(new UserDTO(new UserId(userDetail.getId()), userDetail.getUsername()), page);
+        return ResponseEntity.status(HttpStatus.OK).body(data);
+    }
+}
---
+++ backend/src/test/java/org/example/backend/integration/payment/ReadPaymentTest.java
@@ -0,0 +1,175 @@
+package org.example.backend.integration.payment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.backend.TestDBConfiguration;
+import org.example.backend.domain.payment.model.DebtStatus;
+import org.example.backend.infrastructure.repository.debt.Debt;
+import org.example.backend.infrastructure.repository.debt.JpaDebtRepository;
+import org.example.backend.infrastructure.repository.payment.JpaPaymentRepository;
+import org.example.backend.infrastructure.repository.payment.Payment;
+import org.example.backend.infrastructure.repository.user.JpaUserRepository;
+import org.example.backend.infrastructure.repository.user.User;
+import org.example.backend.infrastructure.security.user.CustomUserDetail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDateTime;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.example.backend.domain.payment.model.PaymentStatus.OPEN;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(TestDBConfiguration.class)
+@Testcontainers
+@ActiveProfiles("test")
+public class ReadPaymentTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JpaPaymentRepository paymentRepository;
+
+    @Autowired
+    private JpaDebtRepository debtRepository;
+
+    @Autowired
+    private JpaUserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private User user1;
+    private User user2;
+    private CustomUserDetail customUserDetail;
+    private Payment payment1;
+    private Payment payment2;
+    private Debt debt1;
+    private Debt debt2;
+
+    @BeforeEach
+    void setup() {
+        debtRepository.deleteAll();
+        paymentRepository.deleteAll();
+        userRepository.deleteAll();
+        user1 = User.builder()
+                .username("Captain")
+                .password("pwd")
+                .build();
+        userRepository.save(user1);
+        user2 = User.builder()
+                .username("Cadet")
+                .password("pwd")
+                .build();
+        userRepository.save(user2);
+        customUserDetail = CustomUserDetail.builder()
+                .id(user1.getId())
+                .username(user1.getUsername())
+                .password(user1.getPassword())
+                .build();
+        payment1 = Payment.builder()
+                .paymentDate(LocalDateTime.now().truncatedTo(SECONDS))
+                .amount(10.)
+                .status(OPEN)
+                .reason("reason1")
+                .isFuelPayment(false)
+                .userId(user1.getId())
+                .build();
+        paymentRepository.save(payment1);
+        payment2 = Payment.builder()
+                .paymentDate(LocalDateTime.now().truncatedTo(SECONDS))
+                .amount(20.)
+                .status(OPEN)
+                .reason("reason1")
+                .isFuelPayment(false)
+                .userId(user2.getId())
+                .build();
+        paymentRepository.save(payment2);
+        debt1 = Debt.builder()
+                .amount(11.)
+                .status(DebtStatus.OPEN)
+                .paymentId(payment1.getId())
+                .userId(user2.getId())
+                .build();
+        debtRepository.save(debt1);
+        debt2 = Debt.builder()
+                .amount(21.)
+                .status(DebtStatus.OPEN)
+                .paymentId(payment2.getId())
+                .userId(user1.getId())
+                .build();
+        debtRepository.save(debt2);
+    }
+
+    @Test
+    void getPaymentsFromLoggedInUserOnSuccess() throws Exception {
+        mockMvc.perform(get("/api/read-payment/all-from-logged-user")
+                        .param("page", "0")
+                        .with(user(customUserDetail)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.length()").value(1))
+                .andExpect(jsonPath("$.hasNext").value(false))
+                .andExpect(jsonPath("$.result[0].paymentId.value").value(payment1.getId()))
+                .andExpect(jsonPath("$.result[0].payedAt").value(payment1.getPaymentDate().toString()))
+                .andExpect(jsonPath("$.result[0].amount").value(payment1.getAmount()))
+                .andExpect(jsonPath("$.result[0].reason").value(payment1.getReason()))
+                .andExpect(jsonPath("$.result[0].isFuelPayment").value(payment1.getIsFuelPayment()))
+                .andExpect(jsonPath("$.result[0].paymentStatus").value(payment1.getStatus().toString()))
+                .andExpect(jsonPath("$.result[0].creditor.userId.value").value(user1.getId()))
+                .andExpect(jsonPath("$.result[0].creditor.username").value(user1.getUsername()))
+                .andExpect(jsonPath("$.result[0].debitors.length()").value(1))
+                .andExpect(jsonPath("$.result[0].debitors[0].debtId.value").value(debt1.getId()))
+                .andExpect(jsonPath("$.result[0].debitors[0].amount").value(debt1.getAmount()))
+                .andExpect(jsonPath("$.result[0].debitors[0].debtStatus").value(debt1.getStatus().toString()))
+                .andExpect(jsonPath("$.result[0].debitors[0].debitor.userId.value").value(debt1.getUserId()))
+                .andExpect(jsonPath("$.result[0].debitors[0].debitor.username").value(user2.getUsername()));
+    }
+
+    @Test
+    void getPaymentsFromLoggedInUserOnPageToBig() throws Exception {
+        mockMvc.perform(get("/api/read-payment/all-from-logged-user")
+                        .param("page", "1")
+                        .with(user(customUserDetail)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.length()").value(0));
+    }
+
+    @Test
+    void getPaymentsFromLoggedInUserOnPageInvalid() throws Exception {
+        mockMvc.perform(get("/api/read-payment/all-from-logged-user")
+                        .param("page", "a")
+                        .with(user(customUserDetail)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Provide a valid Variable"));
+    }
+
+    @Test
+    void getPaymentsFromLoggedInUserOnPageMissing() throws Exception {
+        mockMvc.perform(get("/api/read-payment/all-from-logged-user")
+                        .with(user(customUserDetail)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getPaymentsFromLoggedInUserNotAuthenticated() throws Exception {
+        mockMvc.perform(get("/api/read-payment/all-from-logged-user")
+                        .param("page", "1"))
+                .andExpect(status().isForbidden());
+    }
+}
