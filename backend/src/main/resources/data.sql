83a8ebc20f85a524baed885eb6668f8269faf5d8
Ironmomo <andrin.fassbind@gmail.com>
2025-07-01 09:35:25 +0200

    update schema

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -16,3 +16,12 @@ VALUES ('2025-05-01 08:00:00', '2025-05-01 12:00:00', 100, 104, 1, 1),
        ('2025-05-03 19:00:00', '2025-05-03 20:00:00', 209, 209, 1, 1),
        ('2025-05-04 07:30:00', '2025-05-04 10:30:00', 209, 212, 1, 2),
        ('2025-05-04 14:30:00', '2025-05-04 16:30:00', 212, 214, 1, 1);
+
+INSERT INTO payments (payment_date, amount, status, reason, is_fuel_payment, user_id)
+VALUES ('2025-05-04 14:30:00', 100., 'CHARGED', 'Erste Rechnung', 'false', 1),
+       ('2025-05-04 14:40:00', 200., 'CHARGED', 'Zweite Rechnung ganz eine lange Begründung findet sich hier!', 'false',
+        2);
+
+INSERT INTO debts (amount, status, payment_id, user_id)
+VALUES (48., 'OPEN', 2, 1),
+       (88., 'PAYED', 1, 2)
