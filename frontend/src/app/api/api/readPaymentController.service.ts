3a506874eb73d8e204898d4253052216fd08ec7c
Ironmomo <andrin.fassbind@gmail.com>
2025-07-08 15:07:33 +0200

    autogenerate files

--- frontend/src/app/api/api/readPaymentController.service.ts
+++ frontend/src/app/api/api/readPaymentController.service.ts
@@ -19,6 +19,8 @@ import { Observable }                                        from 'rxjs';
 // @ts-ignore
 import { ExceptionDTO } from '../model/exceptionDTO';
 // @ts-ignore
+import { FuelPaymentPeriodDTO } from '../model/fuelPaymentPeriodDTO';
+// @ts-ignore
 import { PagedResultListDebtPaymentDTO } from '../model/pagedResultListDebtPaymentDTO';
 // @ts-ignore
 import { PagedResultListPaymentUserDTO } from '../model/pagedResultListPaymentUserDTO';
@@ -197,6 +199,63 @@ export class ReadPaymentControllerService extends BaseService {
 
         let localVarPath = `/api/read-payment/all`;
         return this.httpClient.request<PagedResultListPaymentUserDTO>('get', `${this.configuration.basePath}${localVarPath}`,
+            {
+                context: localVarHttpContext,
+                params: localVarQueryParameters,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                transferCache: localVarTransferCache,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * @param boatId 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public getNextInvoicePeriod(boatId: string, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<FuelPaymentPeriodDTO>;
+    public getNextInvoicePeriod(boatId: string, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<HttpResponse<FuelPaymentPeriodDTO>>;
+    public getNextInvoicePeriod(boatId: string, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<HttpEvent<FuelPaymentPeriodDTO>>;
+    public getNextInvoicePeriod(boatId: string, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<any> {
+        if (boatId === null || boatId === undefined) {
+            throw new Error('Required parameter boatId was null or undefined when calling getNextInvoicePeriod.');
+        }
+
+        let localVarQueryParameters = new HttpParams({encoder: this.encoder});
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>boatId, 'boatId');
+
+        let localVarHeaders = this.defaultHeaders;
+
+        const localVarHttpHeaderAcceptSelected: string | undefined = options?.httpHeaderAccept ?? this.configuration.selectHeaderAccept([
+            'application/json'
+        ]);
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        const localVarHttpContext: HttpContext = options?.context ?? new HttpContext();
+
+        const localVarTransferCache: boolean = options?.transferCache ?? true;
+
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        let localVarPath = `/api/read-payment/next-invoice-period`;
+        return this.httpClient.request<FuelPaymentPeriodDTO>('get', `${this.configuration.basePath}${localVarPath}`,
             {
                 context: localVarHttpContext,
                 params: localVarQueryParameters,
--- frontend/src/app/api/api/writePaymentController.service.ts
+++ frontend/src/app/api/api/writePaymentController.service.ts
@@ -20,6 +20,8 @@ import { Observable }                                        from 'rxjs';
 import { CreatePaymentDTO } from '../model/createPaymentDTO';
 // @ts-ignore
 import { ExceptionDTO } from '../model/exceptionDTO';
+// @ts-ignore
+import { FuelInvoiceDTO } from '../model/fuelInvoiceDTO';
 
 // @ts-ignore
 import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables';
@@ -89,6 +91,63 @@ export class WritePaymentControllerService extends BaseService {
             {
                 context: localVarHttpContext,
                 body: createPaymentDTO,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                transferCache: localVarTransferCache,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * @param boatId 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public createInvoice(boatId: string, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<FuelInvoiceDTO>;
+    public createInvoice(boatId: string, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<HttpResponse<FuelInvoiceDTO>>;
+    public createInvoice(boatId: string, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<HttpEvent<FuelInvoiceDTO>>;
+    public createInvoice(boatId: string, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<any> {
+        if (boatId === null || boatId === undefined) {
+            throw new Error('Required parameter boatId was null or undefined when calling createInvoice.');
+        }
+
+        let localVarQueryParameters = new HttpParams({encoder: this.encoder});
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>boatId, 'boatId');
+
+        let localVarHeaders = this.defaultHeaders;
+
+        const localVarHttpHeaderAcceptSelected: string | undefined = options?.httpHeaderAccept ?? this.configuration.selectHeaderAccept([
+            'application/json'
+        ]);
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        const localVarHttpContext: HttpContext = options?.context ?? new HttpContext();
+
+        const localVarTransferCache: boolean = options?.transferCache ?? true;
+
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        let localVarPath = `/api/payment/create-invoice`;
+        return this.httpClient.request<FuelInvoiceDTO>('post', `${this.configuration.basePath}${localVarPath}`,
+            {
+                context: localVarHttpContext,
+                params: localVarQueryParameters,
                 responseType: <any>responseType_,
                 withCredentials: this.configuration.withCredentials,
                 headers: localVarHeaders,
